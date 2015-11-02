22683d456f9d6f98f96bbf9bdf9c93df4b809f34
Tom <tom.forbes@contextis.co.uk>
2015-11-02 11:18:15 +0000

    Add requirements.txt file

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("requirements.txt", "r") as fd:
+    requirements = [line.strip() for line in fd.readlines()]
+
 setup(
     name='cvsslib',
     version='0.1',
@@ -9,6 +12,7 @@ setup(
     author='Tom',
     author_email='tom.forbes@contextis.co.uk',
     description='CVSS 2/3 utilities',
+    install_requires=requirements,
     entry_points={
         'console_scripts': ['cvss=cvsslib.command:main'],
     }
